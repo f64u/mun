48098636fa404be52a6501ba524301aae56da8ba
Fady Adel <2masadel@gmail.com>
2019-11-02 00:57:36 +0200

    yandex verfication

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,16 @@
 import React from "react"
 
+import { Helmet } from "react-helmet"
+
 import s from "./main.module.css"
 import "./layout.css"
 
 export default function Main() {
   return (
     <div className={s.main}>
+      <Helmet>
+        <meta name="yandex-verification" content="274802a862bee6a8" />
+      </Helmet>
       <h1>STEM Model United Nations</h1>
       <h2>Coming Soon.</h2>
     </div>

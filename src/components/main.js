2b338f008c7963de04665a3fcc630b6bb47cb138
Fady Adel <2masadel@gmail.com>
2019-11-01 22:46:35 +0200

    initial commit after some cleanup

---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import s from "./main.module.css"
+import "./layout.css"
+
+export default function Main() {
+  return (
+    <div className={s.main}>
+      <h1>STEM Model United Nations</h1>
+      <h2>Coming Soon.</h2>
+    </div>
+  )
+}
